779c57f8079ec47b0eeaafcede93846e41e31517
arrest <hansl666@126.com>
2023-04-02 21:28:51 +0800

    学习命令执行相关原理。

--- src/main/java/org/example/exec2.java
+++ src/main/java/org/example/exec2.java
@@ -8,8 +8,9 @@ public class exec2 {
     public static void main(String[] args) throws IOException {
         //执行命令
         String[] command = {"/bin/sh","-c","ping -t 3 127.0.0.1;id"};
+        String command2="/bin/sh -c \"ping -t 3 127.0.0.1;id \"";
 //        Process proc = Runtime.getRuntime().exec("ping 127.0.0.1;ls");
-        Process proc = Runtime.getRuntime().exec(command);
+        Process proc = Runtime.getRuntime().exec(command2);
         //回显
         String line;
         BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream(),

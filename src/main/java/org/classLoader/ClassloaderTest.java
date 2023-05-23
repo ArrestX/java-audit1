b8e15845c0a2730b267608d6a9261e999504ca8c
arrest <hansl666@126.com>
2023-05-23 23:03:27 +0800

    学习类加载机制结合反射的使用

--- src/main/java/org/classLoader/ClassloaderTest.java
+++ src/main/java/org/classLoader/ClassloaderTest.java
@@ -1,7 +1,10 @@
 package org.classLoader;
 
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ClassloaderTest {
     public static void main(String[] args) throws Exception{
@@ -14,7 +17,14 @@ public class ClassloaderTest {
 //        System.out.println(systemClassLoader);
 //        Class<?> c = systemClassLoader.loadClass("org.deserialization.Person");
 //        c.newInstance();
-//        new URLClassLoader(new URL[]{new URL("file:")})
+//        URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{new URL("http://192.168.222.184:9999/")});
+//        Class<?> c = urlClassLoader.loadClass("org.classLoader.Test");
+//        c.newInstance();
+        Method defineClassMethod = ClassLoader.class.getDeclaredMethod("defineClass", String.class, byte[].class, int.class, int.class);
+        defineClassMethod.setAccessible(true);
+        byte[] code = Files.readAllBytes(Paths.get("C:\\data\\Test.class"));
+        Class c = (Class) defineClassMethod.invoke(systemClassLoader,"Test",code,0,code.length);
+        c.newInstance();
     }
 
 }
